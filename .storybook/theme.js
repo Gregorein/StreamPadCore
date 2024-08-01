400c8120f3f0ae44de89161b190272fa0e339e32
Gregorein <contact@gregbak.com>
2024-08-01 22:53:06 +0200

    feat: implemented storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,42 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+
+	// typography
+	fontBase: '"Noto Sans", sans-serif',
+	fontCode: 'monospace',
+
+	// branding
+  brandTitle: 'StreampadCore',
+  brandUrl: 'https://streampad.website',
+  brandImage: '.storybook/logo.png',
+  brandTarget: '_self',
+
+	// // theme
+	// colorPrimary: '#3A10E5',
+	// colorSecondary: '#585C6D',
+	
+	// // Text colors
+	// textColor: '#10162F',
+	// textInverseColor: '#ffffff',
+
+	// UI
+	appBg: '#000000',
+	appContentBg: '#111111',
+	appPreviewBg: '#222222',
+	// appBorderColor: '#585C6D',
+	appBorderRadius: 16,
+	
+	// // Toolbar default and active colors
+	// barTextColor: '#9E9E9E',
+	// barSelectedColor: '#585C6D',
+	// barHoverColor: '#585C6D',
+	// barBg: '#ffffff',
+	
+	// // Form colors
+	// inputBg: '#ffffff',
+	// inputBorder: '#10162F',
+	// inputTextColor: '#10162F',
+	// inputBorderRadius: 2,
+});
