f2c0c6a4a50c362d1662544f5677ce82ff1912fe
Gregorein <contact@gregbak.com>
2024-08-16 22:10:51 +0200

    chore: tweaked eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js"
 import globals from "globals"
+import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
@@ -15,10 +16,15 @@ export default tseslint.config({
     "storybook-static",
   ],
   languageOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: "latest",
+    sourceType: "module",
     globals: globals.browser,
   },
+  linterOptions: {
+    reportUnusedDisableDirectives: true,
+  },
   plugins: {
+    "react": react,
     "react-hooks": reactHooks,
     "react-refresh": reactRefresh,
     "streamPad": {
@@ -27,12 +33,26 @@ export default tseslint.config({
       },
     },    
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
+    ...tseslint.configs.recommended.rules,
+    ...react.configs.recommended.rules,
     ...reactHooks.configs.recommended.rules,
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    
+    // "@typescript-eslint/explicit-function-return-type": "warn",
+    // "@typescript-eslint/no-unused-vars": "warn",
+    "no-unused-vars": "off",
+
+    // "@typescript-eslint/explicit-module-boundary-types": "off",
+    // "@typescript-eslint/no-explicit-any": "warn",
 
     "streamPad/enforce-control-schema": "error",
   },
